21b075689e2da9d31af0b0cfdb5f99bcbaacbba1
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-29 21:42:25 +0000

    Update aac_ui.h

--- Client/qtTeamTalk/aac/ui/aac_ui.h
+++ Client/qtTeamTalk/aac/ui/aac_ui.h
@@ -4,12 +4,19 @@
 
 namespace AAC {
 
+class AACGridModel;
+
 class AACUI : public QWidget
 {
     Q_OBJECT
 
 public:
     explicit AACUI(QWidget* parent = nullptr);
+
+    void setModel(AACGridModel* model);
+
+private:
+    AACGridModel* m_model = nullptr;
 };
 
 } // namespace AAC
