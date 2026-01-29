c7b350a97fd86cd7fbae6876b4f5432d6f246135
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-29 21:46:40 +0000

    Update aac_ui.cpp

--- Client/qtTeamTalk/aac/ui/aac_ui.cpp
+++ Client/qtTeamTalk/aac/ui/aac_ui.cpp
@@ -1,18 +1,41 @@
-#include "aac_ui.h"
-
-// TODO: Implement AAC UI logic#pragma once
-
-#include <QWidget>
+#include "aacui.h"
+#include "aac_grid_model.h"
+#include <QGridLayout>
+#include <QPushButton>
 
 namespace AAC {
 
-class AACUI : public QWidget
+AACUI::AACUI(QWidget* parent)
+    : QWidget(parent)
 {
-    Q_OBJECT
+}
 
-public:
-    explicit AACUI(QWidget* parent = nullptr);
-};
+void AACUI::setModel(AACGridModel* model)
+{
+    m_model = model;
+
+    auto* layout = new QGridLayout(this);
+
+    const auto& entries = m_model->entries();
+
+    int row = 0;
+    int col = 0;
+
+    for (const auto& entry : entries) {
+        QPushButton* btn = new QPushButton(entry.label);
+        btn->setIcon(QIcon(entry.iconPath));
+        btn->setIconSize(QSize(64, 64));
+
+        layout->addWidget(btn, row, col);
+
+        col++;
+        if (col == 8) {   // 48-core = 6 rows × 8 columns
+            col = 0;
+            row++;
+        }
+    }
+
+    setLayout(layout);
+}
 
 } // namespace AAC
-
