e10eb50ea5a2892a4db80b80f8e680b5daccee9a
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-29 22:23:48 +0000

    Update aac_module.cpp

--- Client/qtTeamTalk/aac/aac_module.cpp
+++ Client/qtTeamTalk/aac/aac_module.cpp
@@ -1,6 +1,7 @@
 #include "aac_module.h"
 #include "aacui.h"
 #include "aac_grid_model.h"
+#include "aac_message_bar.h"
 
 #include <QFile>
 #include <QJsonDocument>
@@ -12,13 +13,32 @@ namespace AAC {
 AACModule::AACModule()
     : m_ui(new AACUI())
     , m_model(new AACGridModel())
+    , m_messageBar(new AACMessageBar())
 {
     loadCoreVocabulary();
     m_model->loadFromJson(m_coreJson);
 
-    m_ui->setModel(m_model);   // we add this next
+    m_ui->setModel(m_model);
+    connectSignals();
 }
 
+void AACModule::connectSignals()
+{
+    QObject::connect(m_ui, &AACUI::symbolActivated,
+                     this, &AACModule::onSymbolActivated);
+
+    QObject::connect(m_ui, &AACUI::categorySelected,
+                     this, &AACModule::onCategorySelected);
+
+    QObject::connect(m_messageBar, &AACMessageBar::messageReady,
+                     this, &AACModule::onMessageReady);
+}
+
+void AACModule::onCategorySelected(const QString& categoryId)
+{
+    qDebug() << "AACModule: category selected:" << categoryId;
+    // later: load different JSON section into m_model and call m_ui->setModel(m_model) again
+}
 void AACModule::loadCoreVocabulary()
 {
     QFile file(":/aac/core48.json");
@@ -40,6 +60,16 @@ void AACModule::loadCoreVocabulary()
     }
 
     m_coreJson = doc.object();
+}
+void AACModule::onSymbolActivated(const QString& label)
+{
+    m_messageBar->appendSymbol(label);
+}
+
+void AACModule::onMessageReady(const QString& text)
+{
+    qDebug() << "AACModule: message ready:" << text;
+    // later: hook into TTS / sending
 }
 
 } // namespace AAC
