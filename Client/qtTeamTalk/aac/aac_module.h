b2cc5f976fe44a24121d810a32207a5b2872a9c4
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-29 22:14:02 +0000

    Update aac_module.h

--- Client/qtTeamTalk/aac/aac_module.h
+++ Client/qtTeamTalk/aac/aac_module.h
@@ -8,6 +8,7 @@ class AACUI;
 namespace AAC {
 
 class AACGridModel;
+class AACMessageBar;
 
 class AACModule : public QObject
 {
@@ -18,9 +19,15 @@ public:
 
 private:
     void loadCoreVocabulary();
+    void connectSignals();
+
+private slots:
+    void onSymbolActivated(const QString& label);
+    void onMessageReady(const QString& text);
 
     AACUI* m_ui;
     AACGridModel* m_model;
+    AACMessageBar* m_messageBar;
     QJsonObject m_coreJson;
 };
 
