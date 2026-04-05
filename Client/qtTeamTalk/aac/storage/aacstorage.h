79e4fa6def99d34e0456d829fdc2995e7b7ebc2a
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-04-05 22:41:46 +0100

    Update aacstorage.h

--- Client/qtTeamTalk/aac/storage/aacstorage.h
+++ Client/qtTeamTalk/aac/storage/aacstorage.h
@@ -1,46 +1,21 @@
 #pragma once
 
-#include <QList>
 #include <QString>
-#include <QByteArray>
-#include <QDateTime>
-#include "Client/qtTeamTalk/aac/models/serverinfo.h"
+#include <QJsonObject>
+
+class AACAccessibilityManager;
 
 class AACStorage
 {
 public:
     AACStorage();
 
-    QList<ServerInfo> loadPersonalServers() const;
-    void savePersonalServers(const QList<ServerInfo>& servers);
-    QList<ServerInfo> loadLatestHosts() const;
-    void saveLatestHosts(const QList<ServerInfo>& hosts);
-
-QVariantList numbers() const;
-QVariantList places() const;
-QVariantList coreSymbols() const;
-bool loadPublicCache(QByteArray& xml, QDateTime& timestamp) const;
-    void savePublicCache(const QByteArray& xml, const QDateTime& timestamp);
+    void hydrate(AACAccessibilityManager& mgr);
+    void persist(const AACAccessibilityManager& mgr);
 
 private:
-    QString settingsGroupPersonal() const { return QStringLiteral("aac/personal_servers"); }
-    QString settingsGroupLatest()   const { return QStringLiteral("aac/latest_hosts"); }
-    QString cacheFilePath() const;
-    QString cacheMetaPath() const;
-QString aacDataDir() const;
-QString numbersPath() const;
-QString placesPath() const;
-QString coreSymbolsPath() const;
-QVariantList loadJsonArray(const QString& path) const;
+    QString storagePath() const;
 
- mutable QVariantList m_numbersCache;
-mutable QVariantList m_placesCache;
-mutable QVariantList m_coreSymbolsCache;
-
-mutable bool m_numbersLoaded = false;
-mutable bool m_placesLoaded = false;
-mutable bool m_coreLoaded = false;
-
-ServerInfo fromSettings(const QString& prefix) const;
-    void toSettings(const QString& prefix, const ServerInfo& info) const;
+    QJsonObject loadJson() const;
+    void saveJson(const QJsonObject& obj) const;
 };
