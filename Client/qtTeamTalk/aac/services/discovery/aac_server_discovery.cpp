d7a573b382e7db29bac1ced6a955f81f097d500d
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-04-07 14:10:50 +0100

    Update aac_server_discovery.cpp

--- Client/qtTeamTalk/aac/services/discovery/aac_server_discovery.cpp
+++ Client/qtTeamTalk/aac/services/discovery/aac_server_discovery.cpp
@@ -1,21 +1,22 @@
 #include "aac_server_discovery.h"
 #include "serverservice.h"
 
-#include <QDebug>
+#include <QDomDocument>
+#include <QHostAddress>
+#include <QNetworkInterface>
 
 AACServerDiscovery::AACServerDiscovery(ServerService* service,
                                        QObject* parent)
     : QObject(parent)
     , m_service(service)
 {
-    connect(&m_lanTimer, &QTimer::timeout,
-            this, &AACServerDiscovery::onLANDiscoveryTick);
 }
 
 void AACServerDiscovery::setState(State s)
 {
     if (m_state == s)
         return;
+
     m_state = s;
     emit stateChanged(s);
 }
@@ -32,22 +33,23 @@ void AACServerDiscovery::startDiscovery()
 
     setState(State::Discovering);
 
-    // 1) Emit cached servers immediately
+    // 1) Cached servers first (instant feedback)
     emitCachedServers();
 
-    // 2) Fetch public servers (async)
-    m_service->fetchPublicServers(true);
+    // 2) Public servers (async)
     connect(m_service, &ServerService::publicServersUpdated,
             this, [this](const QList<ServerInfo>& list) {
-                for (const auto& s : list) {
-                    if (!m_seen.contains(s)) {
-                        m_seen.append(s);
-                        emit serverFound(s);
-                    }
-                }
-            });
+        for (const auto& s : list) {
+            if (!m_seen.contains(s)) {
+                m_seen.append(s);
+                emit serverFound(s);
+            }
+        }
+    });
 
-    // 3) Begin LAN discovery
+    m_service->fetchPublicServers(true);
+
+    // 3) LAN discovery
     beginLANDiscovery();
 }
 
@@ -75,9 +77,11 @@ void AACServerDiscovery::selectServer(const ServerInfo& info)
 void AACServerDiscovery::emitCachedServers()
 {
     QList<ServerInfo> cached = m_storage.loadLatestHosts();
-    for (auto& s : cached) {
+    for (auto s : cached) {
         s.source = ServerSource::Cached;
+        s.lastSeen = QDateTime::currentDateTimeUtc();
         s.updateId();
+
         m_seen.append(s);
         emit serverFound(s);
     }
@@ -85,24 +89,64 @@ void AACServerDiscovery::emitCachedServers()
 
 void AACServerDiscovery::beginLANDiscovery()
 {
-    // LAN discovery is simulated here; you will replace this
-    // with TeamTalk's UDP broadcast listener.
-    m_lanTimer.start(1500);
+    if (!m_udp) {
+        m_udp = new QUdpSocket(this);
+
+        connect(m_udp, &QUdpSocket::readyRead,
+                this, [this] {
+            while (m_udp->hasPendingDatagrams()) {
+                QByteArray datagram;
+                datagram.resize(int(m_udp->pendingDatagramSize()));
+                m_udp->readDatagram(datagram.data(), datagram.size());
+                handleLANResponse(datagram);
+            }
+        });
+    }
+
+    // Broadcast "TTLAN" to all interfaces
+    QByteArray request("TTLAN");
+
+    for (const QNetworkInterface& iface : QNetworkInterface::allInterfaces()) {
+        if (!(iface.flags() & QNetworkInterface::IsUp))
+            continue;
+        if (!(iface.flags() & QNetworkInterface::IsRunning))
+            continue;
+        if (iface.flags() & QNetworkInterface::IsLoopBack)
+            continue;
+
+        for (const QNetworkAddressEntry& entry : iface.addressEntries()) {
+            QHostAddress broadcast = entry.broadcast();
+            if (!broadcast.isNull()) {
+                m_udp->writeDatagram(request, broadcast, 10333);
+            }
+        }
+    }
 }
 
 void AACServerDiscovery::endLANDiscovery()
 {
-    m_lanTimer.stop();
+    if (m_udp) {
+        m_udp->close();
+        m_udp->deleteLater();
+        m_udp = nullptr;
+    }
 }
 
-void AACServerDiscovery::onLANDiscoveryTick()
+void AACServerDiscovery::handleLANResponse(const QByteArray& datagram)
 {
-    // Placeholder LAN discovery result
+    QDomDocument doc;
+    if (!doc.setContent(datagram))
+        return;
+
+    QDomElement root = doc.documentElement();
+    QDomElement hostElem = root.firstChildElement("host");
+    if (hostElem.isNull())
+        return;
+
     ServerInfo info;
-    info.host = "192.168.1.100";
-    info.tcpPort = 10333;
-    info.udpPort = 10333;
-    info.name = "LAN Server";
+    if (!m_service->parseServerElement(hostElem, info))
+        return;
+
     info.source = ServerSource::LAN;
     info.lastSeen = QDateTime::currentDateTimeUtc();
     info.updateId();
