271c45a2a6fd51e10f6f5b8a239bcc0ac6a1283d
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-04-08 14:45:11 +0100

    AAC Cleanup: removed legacy TeamTalk UI (dialogs, views, server widgets)

--- Client/qtTeamTalk/aac/ui/AudioBanner.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QWidget>
-
-class AudioBanner : public QWidget {
-    Q_OBJECT
-public:
-    explicit AudioBanner(QWidget* parent = nullptr);
-    void showMessage(const QString& msg);
-};
